a23ee4f3b010d447c1aa2982f9279f21bbae0448
phoenix1512 <39445352+phoenix1512@users.noreply.github.com>
2020-03-24 19:56:41 -0700

    Add files via upload
    ejercicio 11

---
+++ ejecicio 11/map.js
@@ -0,0 +1,8 @@
+var numbers = [1, 5, 10, 15];
+var doubles = numbers.map(function(x) {
+   return x * 2;
+});
+var numbers = [1, 4, 9];
+var roots = numbers.map(function(num) {
+    return Math.sqrt(num);
+});
